3de82bb89bef768eaadb1112504b7ea1814d4bdf
alaattin-duzdere <aleykedu@gmail.com>
2025-09-28 14:57:41 +0300

    CustomAccesDeniedHandler and AuthEntryPoint added. Now on 401 and 403 errors seperated away.

--- backend/src/main/java/com/example/marketplace_backend/MarketplaceBackendApplication.java
+++ backend/src/main/java/com/example/marketplace_backend/MarketplaceBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.marketplace_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class MarketplaceBackendApplication {
 
 	public static void main(String[] args) {
